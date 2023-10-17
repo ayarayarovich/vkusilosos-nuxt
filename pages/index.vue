16cfb2c52b674c28a9c3abbaa33c68e9c48f1c89
Айяр Халилов <super.ayar-04@yandex.ru>
2023-10-17 12:39:31 +0300

    kapec

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,18 @@
 import type { Category } from '~/interfaces/dishes'
 import { useCategoryStore } from '~/store/category'
 import { storeToRefs } from 'pinia'
-
+import ky from 'ky'
 
 const categoryStore = useCategoryStore()
 const { categories } = storeToRefs(categoryStore)
 
-useFetch<Category[]>('https://api.losos.ayarayarovich.tech/api/categories').then((res) => categories.value = res.data.value)
+useFetch<Category[]>('https://api.losos.ayarayarovich.tech/api/categories', {key: 'categories'}).then((res) => categories.value = res.data.value)
+
+// useAsyncData<Category[]>('categories', () => ky.get('https://api.losos.ayarayarovich.tech/api/categories').json()).then(data => {
+//     categories.value = data.data.value
+// })
+
+
 </script>
 
 <template>
