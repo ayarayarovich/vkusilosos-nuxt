170bbab55b34ee3653ed7cee2cf81427daa7fb52
Айяр Халилов <super.ayar-04@yandex.ru>
2023-12-19 10:52:40 +0300

    update phone formatting

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 export const formatPhone = (phone: string) => {
-  return phone.replace(/\D+/g, '').replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1 ($2) $3-$4-$5')
+  return phone.replace(/\D+/g, '').replace(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1 ($2) $3 - $4 - $5')
 }
 
 export function pad(n: number, size: number) {
