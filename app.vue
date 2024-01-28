e34ec339cb6c5f3983eb88c4b86a554dbf6be5bd
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-01-28 12:11:36 +0300

    add yandex metrica

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <NuxtLayout>
+    <noscript>
+      <div>
+        <img
+          src="https://mc.yandex.ru/watch/94201196"
+          style="position: absolute; left: -9999px"
+          alt=""
+        />
+      </div>
+    </noscript>
     <NuxtPage />
   </NuxtLayout>
 </template>
@@ -10,5 +19,23 @@ import 'vue-toastification/dist/index.css'
 useHead({
   title: 'ВкусиЛосось',
   link: [{ rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }],
+  script: [
+    {
+      children: `
+   (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(94201196, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true,
+        ecommerce:"dataLayer"
+   });`,
+    },
+  ],
 })
 </script>
