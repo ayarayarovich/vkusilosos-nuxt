b8bea95e9b23822e1ab62cb9a1c085296ecbcd68
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-01-31 15:12:41 +0300

    use code for access recovery

--- components/AuthDialog/PasswordRecoveryView/ResendLink.vue
+++ components/AuthDialog/PasswordRecoveryView/ResendLink.vue
@@ -2,9 +2,14 @@
   <div class="flex h-full flex-col items-stretch justify-between py-6">
     <div>
       <p class="mb-4">
-        Мы отправили вам ссылку для восстановления пароля на почту. Если вы не получили ссылку, то нажмите на кнопку
+        Мы отправили вам код для восстановления пароля на почту. Если вы не получили код, то нажмите на кнопку
         “Отправить повторно”
       </p>
+
+      <PinInput
+        name="code"
+        :length="4"
+      />
     </div>
 
     <SimpleButton
@@ -17,13 +22,58 @@
 </template>
 
 <script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query'
+import * as yup from 'yup'
+
 const props = defineProps<{
   email: string
 }>()
 
-const { mutateAsync } = useSendRecovery()
+useForm({
+  validationSchema: yup.object({
+    code: yup.string().length(4).required().label('Код'),
+  }),
+})
+
+const code = useFieldValue<string | undefined>('code')
+
+const fbToken = useFCMToken()
+
+const publicAxios = usePublicAxiosInstance()
+const { mutateAsync } = useMutation({
+  mutationFn: async (vals: { email: string; code: string }) => {
+    const response = await publicAxios.post<{
+      token: string
+      refreshToken: string
+    }>('auth/verify', {
+      login: vals.email,
+      code: vals.code,
+      fb_token: fbToken.value,
+    })
+    return response.data
+  },
+})
+
+const { userCredentials } = useUserCredentials()
+
+watchEffect(() => {
+  if (code.value?.length === 4) {
+    mutateAsync({
+      email: props.email,
+      code: code.value,
+    }).then((r) => {
+      userCredentials.value = {
+        accessToken: r.token,
+        refreshToken: r.refreshToken,
+        isAuthenticated: true,
+      }
+    })
+  }
+})
+
+const { mutateAsync: resendRecovery } = useSendRecovery()
 
 const send = () => {
-  mutateAsync({ email: props.email })
+  resendRecovery({ email: props.email })
 }
 </script>
