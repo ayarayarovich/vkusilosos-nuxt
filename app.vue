2ac27966f0e83e36923dcdf5c68b8f64870daa8b
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-03-23 09:48:08 +0300

    update yandex metrika

--- app.vue
+++ app.vue
@@ -1,14 +1,5 @@
 <template>
   <NuxtLayout>
-    <noscript>
-      <div>
-        <img
-          src="https://mc.yandex.ru/watch/94201196"
-          style="position: absolute; left: -9999px"
-          alt=""
-        />
-      </div>
-    </noscript>
     <NuxtPage />
   </NuxtLayout>
 </template>
@@ -22,7 +13,7 @@ useHead({
   script: [
     {
       children: `
-   (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();
    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -35,6 +26,12 @@ useHead({
         webvisor:true,
         ecommerce:"dataLayer"
    });`,
+    },
+  ],
+  noscript: [
+    {
+      innerHTML:
+        '<div><img src="https://mc.yandex.ru/watch/94201196" style="position:absolute; left:-9999px;" alt="" /></div>',
     },
   ],
 })
